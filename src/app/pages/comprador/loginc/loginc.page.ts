30e2ddfab32bebe990ae7f3fab45081d9c707f02
MarCox02 <Cox.Martin2002@gmail.com>
2024-08-26 22:32:09 -0400

    Registro de Compradores 0.4

--- src/app/pages/comprador/loginc/loginc.page.ts
+++ src/app/pages/comprador/loginc/loginc.page.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-loginc',
   templateUrl: './loginc.page.html',
   styleUrls: ['./loginc.page.scss'],
 })
-export class LogincPage implements OnInit {
+export class LogincPage{
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
---
+++ src/app/pages/comprador/registrarc/registrarc.page.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-registrarc',
+  templateUrl: './registrarc.page.html',
+  styleUrls: ['./registrarc.page.scss'],
+})
+export class RegistrarcPage {
+
+  constructor() { }
+
+}
