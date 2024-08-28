354698f1acc753003c198bc21abb5b0f20367349
MarCox02 <Cox.Martin2002@gmail.com>
2024-08-27 22:18:52 -0400

    Publicar productos creada 0.50

---
+++ src/app/pages/vendedor/publicarv/publicarv.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { PublicarvPageRoutingModule } from './publicarv-routing.module';
+
+import { PublicarvPage } from './publicarv.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    PublicarvPageRoutingModule
+  ],
+  declarations: [PublicarvPage]
+})
+export class PublicarvPageModule {}
--- src/app/pages/vendedor/publicarv/publicarv.page.ts
+++ src/app/pages/vendedor/publicarv/publicarv.page.ts
@@ -2,17 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { MenuController } from '@ionic/angular';
 
 @Component({
-  selector: 'app-registrarc',
-  templateUrl: './registrarc.page.html',
-  styleUrls: ['./registrarc.page.scss'],
+  selector: 'app-publicarv',
+  templateUrl: './publicarv.page.html',
+  styleUrls: ['./publicarv.page.scss'],
 })
-export class RegistrarcPage implements OnInit{
+export class PublicarvPage implements OnInit {
 
   constructor(private menuCtrl: MenuController) {}
 
-  ngOnInit() {
-    this.menuCtrl.enable(false,'vendedor')
-    this.menuCtrl.enable(false,'comprador')
 
+  ngOnInit() {
+    this.menuCtrl.enable(false,'comprador')
+    this.menuCtrl.enable(true,'vendedor')
   }
 }
