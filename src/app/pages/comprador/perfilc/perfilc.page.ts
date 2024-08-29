c54e0982e86b70e745a2e2da127540a059513f3f
angel <an.perugini@duocuc.cl>
2024-08-29 19:01:43 -0400

    Creando perfil c

---
+++ src/app/pages/comprador/perfilc/perfilc.page.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-perfilc',
+  templateUrl: './perfilc.page.html',
+  styleUrls: ['./perfilc.page.scss'],
+})
+export class PerfilcPage implements OnInit {
+
+  constructor(private menuCtrl: MenuController) { }
+
+  ngOnInit() {
+    this.menuCtrl.enable(true,'comprador')
+    this.menuCtrl.enable(false,'vendedor')
+  }
+
+}
