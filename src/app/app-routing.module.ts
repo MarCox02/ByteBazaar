b493000ee893b068f1ac8b7655276b33f7e4076a
MarCox02 <Cox.Martin2002@gmail.com>
2024-08-29 20:43:36 -0400

    Menus Mejorados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -39,7 +39,7 @@ const routes: Routes = [
     path: 'historial-productos',
     loadChildren: () => import('./pages/vendedor/historial-productos/historial-productos.module').then( m => m.HistorialProductosPageModule)
   },
-  
+
   {path: 'publicarv',
     loadChildren: () => import('./pages/vendedor/publicarv/publicarv.module').then( m => m.PublicarvPageModule)},
 
@@ -56,9 +56,6 @@ const routes: Routes = [
     loadChildren: () => import('./pages/comprador/perfilc/perfilc.module').then( m => m.PerfilcPageModule)
   },
   {
-    path: '**',
-    loadChildren: () => import('./pages/not-found/not-found.module').then( m => m.NotFoundPageModule)
-  },  {
     path: 'historial-compras',
     loadChildren: () => import('./pages/comprador/historial-compras/historial-compras.module').then( m => m.HistorialComprasPageModule)
   },
@@ -66,8 +63,10 @@ const routes: Routes = [
     path: 'carrito',
     loadChildren: () => import('./pages/comprador/carrito/carrito.module').then( m => m.CarritoPageModule)
   },
-
-
+  {
+    path: '**',
+    loadChildren: () => import('./pages/not-found/not-found.module').then( m => m.NotFoundPageModule)
+  },
 
 ];
 
