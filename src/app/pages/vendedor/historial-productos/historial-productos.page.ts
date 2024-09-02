02f3794b8d1382bdb3bb39eaf918d406e7a348d0
MarCox02 <Cox.Martin2002@gmail.com>
2024-09-02 16:17:32 -0400

    historiales Con For

--- src/app/pages/vendedor/historial-productos/historial-productos.page.ts
+++ src/app/pages/vendedor/historial-productos/historial-productos.page.ts
@@ -7,6 +7,12 @@ import { MenuController } from '@ionic/angular';
   styleUrls: ['./historial-productos.page.scss'],
 })
 export class HistorialProductosPage implements OnInit {
+  public productos = [
+    {id: '12LNVDSKT320',nombre:'Laptop Lenovo',color:'color:rgb(51, 90, 218);',estado:'En Ruta',img:'/assets/computador lenovo.jpg',},
+    {id: 'M432LGTMUSSEGGM',nombre:'Logitech mouse Gamer',color:'color:rgb(211, 0, 0)',estado:'Compra Cancelada',img:'/assets/mouselogitex.jpg'},
+    {id: 'G413LGTTKLSEMGK',nombre:'Logitech teclado mecanico',color:'color:rgb(0, 128, 0);',estado:'Entregado',img:'/assets/teclado.jpg'},
+
+  ]
 
   constructor(private menuCtrl: MenuController) { }
 
