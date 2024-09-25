707cc0c89e1355185a44f0026fad410626b5a53a
MarCox02 <Cox.Martin2002@gmail.com>
2024-09-24 22:47:31 -0300

    UPD ICONO

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,8 +2,9 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'devMovil',
-  webDir: 'www'
+  appName: 'ByteBazaar',
+  webDir: 'www',
+
 };
 
 export default config;
